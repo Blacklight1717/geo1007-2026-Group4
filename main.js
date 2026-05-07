0410976bffdefe30df24e233b19871e778941b94
Niranjan Pradeep <niranjan.pradeep1910@gmail.com>
2026-05-07 15:49:31 +0200

    changes

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ map.attributionControl.setPrefix('');
 
 // BRT - (Base Registry Topography) BaseMap PDOK:
 let options = { maxZoom: 14, attribution: 'Map data: <a href="http://www.pdok.nl">BRT Achtergrondkaart</a>' }
-let basemap_pdok = new L.tileLayer('https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/standaard/EPSG:4326/{z}/{x}/{y}.png', options);
+let basemap_pdok = new L.tileLayer('https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/standaard/EPSG:28992/{z}/{x}/{y}.png', options);
 
 basemap_pdok.getAttribution = function () {
   return 'BRT Achtergrondkaart <a href="http://www.kadaster.nl">Kadaster</a>.';
